22f009290d3f1c2af619506d80b52f3fcd970213
HiHi3690 <duimstrajakson@gmail.com>
2025-01-07 19:46:04 -0500

    done :3

--- src/main/java/frc/robot/subsystems/intake/pivot/PivotIOSim.java
+++ src/main/java/frc/robot/subsystems/intake/pivot/PivotIOSim.java
@@ -1,16 +1,30 @@
 package frc.robot.subsystems.intake.pivot;
 
 import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.system.plant.DCMotor;
 import edu.wpi.first.wpilibj.simulation.SingleJointedArmSim;
+import frc.robot.Constants;
 
 public class PivotIOSim implements PivotIO {
   // For instructions on how to implement this class, refer to the README.md file
 
   private SingleJointedArmSim m_sim;
-  // define more members here as necessary
+  private DCMotor m_motor;
+  private double m_voltage;
 
   public PivotIOSim() {
-    // TODO: Implement this constructor
+    m_motor = DCMotor.getNEO(1);
+    m_sim =
+        new SingleJointedArmSim(
+            m_motor,
+            Constants.IntakeConstants.kPivotGearing,
+            Constants.IntakeConstants.kPivotJKgMetersSquared,
+            Constants.IntakeConstants.kPivotLength,
+            Constants.IntakeConstants.kPivotMinAngle,
+            Constants.IntakeConstants.kPivotMaxAngle,
+            false,
+            2 * Math.PI / 3);
+    m_voltage = 0;
   }
 
   @Override
@@ -24,24 +38,22 @@ public class PivotIOSim implements PivotIO {
 
   @Override
   public void setVoltage(double voltage) {
-    // TODO: Implement this method
+    m_sim.setInput(voltage);
+    m_voltage = voltage;
   }
 
   @Override
   public double getVoltage() {
-    // TODO: Implement this method
-    return 0.0;
+    return m_voltage;
   }
 
   @Override
   public double getVelocityRadPerSec() {
-    // TODO: Implement this method
-    return 0.0;
+    return m_sim.getVelocityRadPerSec();
   }
 
   @Override
   public Rotation2d getAngle() {
-    // TODO: Implement this method
-    return null;
+    return new Rotation2d(m_sim.getAngleRads());
   }
 }
